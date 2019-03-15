71a563b2e0ce815f58db3be28a2593c624e7a002
Jorge Cisneros <fraxito@gmail.com>
2019-03-15 12:39:23 +0100

    Ahora La nave se mueve a izquierda y derecha perfectamente

--- src/codigo/Nave.java
+++ src/codigo/Nave.java
@@ -17,6 +17,8 @@ public class Nave {
     public Image imagen = null;
     public int x = 0;
     public int y = 0;
+    private boolean pulsadoIzquierda = false;
+    private boolean pulsadoDerecha = false;
     
     public Nave(){
         try {
@@ -24,5 +26,32 @@ public class Nave {
         } catch (IOException ex) {
             
         }
+    }
+    
+    public void mueve(){
+        if (pulsadoIzquierda){
+            x--;
+        }
+        if (pulsadoDerecha){
+            x++;
+        }  
+    }
+
+    public boolean isPulsadoIzquierda() {
+        return pulsadoIzquierda;
+    }
+
+    public void setPulsadoIzquierda(boolean pulsadoIzquierda) {
+        this.pulsadoIzquierda = pulsadoIzquierda;
+        this.pulsadoDerecha = false;
+    }
+
+    public boolean isPulsadoDerecha() {
+        return pulsadoDerecha;
+    }
+
+    public void setPulsadoDerecha(boolean pulsadoDerecha) {
+        this.pulsadoDerecha = pulsadoDerecha;
+        this.pulsadoIzquierda = false;
     }
 }
--- src/codigo/VentanaJuego.java
+++ src/codigo/VentanaJuego.java
@@ -9,6 +9,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import javax.swing.Timer;
 
@@ -58,7 +59,7 @@ public class VentanaJuego extends javax.swing.JFrame {
         ///////////////////////////////////////////////////////
         //redibujaremos aquí cada elemento
         g2.drawImage(miNave.imagen, miNave.x, miNave.y, null);
-        
+        miNave.mueve();
         
         /////////////////////////////////////////////////////////////
         //*****************   fase final, se dibuja ***************//
@@ -82,6 +83,14 @@ public class VentanaJuego extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                formKeyPressed(evt);
+            }
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                formKeyReleased(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -107,6 +116,20 @@ public class VentanaJuego extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
+       switch (evt.getKeyCode()){
+           case KeyEvent.VK_LEFT: miNave.setPulsadoIzquierda(true) ; break;
+           case KeyEvent.VK_RIGHT: miNave.setPulsadoDerecha(true); break;
+       }
+    }//GEN-LAST:event_formKeyPressed
+
+    private void formKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyReleased
+       switch (evt.getKeyCode()){
+           case KeyEvent.VK_LEFT: miNave.setPulsadoIzquierda(false) ; break;
+           case KeyEvent.VK_RIGHT: miNave.setPulsadoDerecha(false); break;
+       }
+    }//GEN-LAST:event_formKeyReleased
 
     /**
      * @param args the command line arguments
