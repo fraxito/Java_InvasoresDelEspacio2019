849ef531f2b02c2b3f88127a328be802d34a5fa1
Jorge Cisneros <fraxito@gmail.com>
2019-03-22 12:45:02 +0100

    Con el spriteSheet cargado funciona OK

--- src/codigo/VentanaJuego.java
+++ src/codigo/VentanaJuego.java
@@ -7,6 +7,7 @@ package codigo;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -42,7 +43,7 @@ public class VentanaJuego extends javax.swing.JFrame {
     int contador = 0;
     //imagen para cargar el spritesheet con todos los sprites del juego
     BufferedImage plantilla = null;
-    BufferedImage [] imagenes = new BufferedImage[30];
+    Image [] imagenes = new Image[30];
     
     Timer temporizador = new Timer(10, new ActionListener() {
         @Override
@@ -61,9 +62,10 @@ public class VentanaJuego extends javax.swing.JFrame {
         } catch (IOException ex) {            
         }
         //cargo las imagenes de forma individual en cada imagen del array de imagenes
-        for (int i=0; i<6; i++){
-            for (int j=0; j<5; j++){
-                imagenes[i*5 + j] = plantilla.getSubimage(j*32, i*32, 32, 32);
+        for (int i=0; i<5; i++){
+            for (int j=0; j<4; j++){
+                imagenes[i*4 + j] = plantilla.getSubimage(j*64, i*64, 64, 64);
+                imagenes[i*4 + j] = imagenes[i*4 + j].getScaledInstance(32, 32, Image.SCALE_SMOOTH);
             }
         }
         
@@ -77,13 +79,13 @@ public class VentanaJuego extends javax.swing.JFrame {
         //inicializo la posición inicial de la nave
         miNave.x = ANCHOPANTALLA / 2 - miNave.imagen.getWidth(this) / 2;
         miNave.y = ALTOPANTALLA - miNave.imagen.getHeight(this) - 40;
-
+        miNave.imagen = imagenes[0];
         //inicializo el array de marcianos
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
                 listaMarcianos[i][j] = new Marciano();
-                listaMarcianos[i][j].imagen1 = imagenes[2];
-                listaMarcianos[i][j].imagen2 = imagenes[3];
+                listaMarcianos[i][j].imagen1 = imagenes[4];
+                listaMarcianos[i][j].imagen2 = imagenes[5];
                 listaMarcianos[i][j].x = j * (15 + listaMarcianos[i][j].imagen1.getWidth(null));
                 listaMarcianos[i][j].y = i * (10 + listaMarcianos[i][j].imagen1.getHeight(null));
             }
